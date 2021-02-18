718406d7b445871ea8c23cad0867e58aad99bb9c
eyal shemuel <eshemuel1@gmail.com>
2021-02-18 19:57:36 +0200

    eyal

---
+++ React/Timers/eyal-timer/src/App.js
@@ -0,0 +1,50 @@
+import {useState} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+function App() {
+  const [tasks,setTasks] = useState(
+    [
+         {
+             id: 1,
+             text: 'Doctor Appoinment',
+             day: 'feb 5th at 2:30pm',
+             remainder: true,
+         },
+         {
+             id: 2,
+             text: 'Meeting at School',
+             day: 'feb 6th at 2:30pm',
+             remainder: true,
+         },
+         {
+             id: 3,
+             text: 'Food Shopping',
+             day: 'feb 7th at 2:30pm',
+             remainder: false,
+         }
+     ]
+ )
+//Delete tasks
+const deleteTask = (id) => {
+  setTasks(
+    tasks.filter((task)=>task.id !== id)
+  )
+}
+
+//Toggle Remainder
+const toggleRemainder = (id)=>{
+console.log('Toggle Remainder')
+setTasks(
+  tasks.map((task)=>task.id !== id ?{...task,remainder:!task.remainder}:task)
+)
+}
+
+  return (
+    <div className='container'>
+     <Header />
+    {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRemainder}/> : 'No tasks to Show'}
+    </div>
+  )
+}
+
+export default App;
