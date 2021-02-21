a257fe5ec4dfc64bd31fd14de3fe1e1626d00f05
hillel <a4105962@gmail.com>
2021-02-21 15:31:52 +0200

    cont

--- React/Timers/hillel/src/timer.js
+++ React/Timers/hillel/src/timer.js
@@ -1,63 +1,58 @@
 import React, { useState } from 'react';
 import './App.css';
-
+import Table from './Table'
 
 const timers = [
     {
         text: "בוקר",
-        time: "07:30:00"
+        time: "07:30:00",
+        color: 'gray'
     }, {
         text: "נסיעה",
-        time: "20:31:55"
+        time: "20:31:55",
+        color: 'gray'
     }
 ]
 
 
+let date = new Date().toLocaleTimeString()
 function App() {
     const [timer, setThhimer] = useState(timers)
+    const [divler, settimet] = useState(date)
 
     function settimer(e) {
         e.preventDefault();
 
-        const text = e.target.children[0].value;
-        const time = e.target.children[1].value;
-
-        timers.push({ text: text, time: time })
+        const text = e.target.children.text.value;
+        const time = e.target.children.timerstext.value;
+        const color = 'gray'
+        timers.push({ text, time, color })
         setThhimer(timers)
         // e.target.reset();
     }
 
+
+    setInterval(function () {
+        date = new Date().toLocaleTimeString()
+        settimet(date)
+    }, 500);
+
+
     return (
         <>
+            <div className='divler'>
+                {divler}
+            </div>
             <form onSubmit={settimer}>
-                <input type="text" id='text' />
-                <input type="time" id='time' step='1' />
+                <input type="text" id='text' name='text' />
+                <input type="time" id='time' step='1' name='timerstext' />
                 <button type='submit'>הוסף תזכורת</button>
             </form>
             <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>הערת תזכורת</th>
-                            <th>שעה</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {timer.map((elm, index) =>
-                            <tr key={index}>
-                                {console.log('c')}
-                                <td>{elm.text}</td>
-                                <td>{elm.time}</td>
-                            </tr>
-                        )}
-                    </tbody>
-                </table>
+                <Table array={timer} />
             </div>
         </>
     );
 }
 
 export default App;
-
-// {timer.map((elm, index) =>
-//     <p key={index}>{elm.text}</p>
