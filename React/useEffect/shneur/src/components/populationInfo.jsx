789432640252ec4e5d0c8e074afb280691f3d0fa
shneur <770abhtur1@gmail.com>
2021-03-01 18:06:21 +0200

    xcvb

--- React/useEffect/shneur/src/components/populationInfo.jsx
+++ React/useEffect/shneur/src/components/populationInfo.jsx
@@ -1,23 +1,8 @@
 import React from "react";
 import "../App.css";
 
+
 const PopulationInfo = ({}) => {
-  fetch(
-    "https://world-population.p.rapidapi.com/population?country_name=Israel",
-    {
-      method: "GET",
-      headers: {
-        "x-rapidapi-key": "e846b0c7f4msha2c3878c64cee80p17095djsn12b3512845a6",
-        "x-rapidapi-host": "world-population.p.rapidapi.com",
-      },
-    }
-  )
-    .then((data) => {
-      console.log({...data});
-    })
-    .catch((err) => {
-      console.error(err);
-    });
   return (
     <>
       <h1>On this day...</h1>
