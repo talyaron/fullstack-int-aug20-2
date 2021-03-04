0d217008b8a585b9aba2f56a8b6d7e2c603fc375
shneur <770abhtur1@gmail.com>
2021-03-04 17:19:19 +0200

    ghjkl

---
+++ React/project/shneur/shneur/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Categories from 'Categories';
+
+
+function App() {
+  return (
+    <>
+    <Categories/>
+ 
+    </>
+  );
+}
+
+export default App;
---
+++ React/project/shneur/shneur/src/Categories.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function Categories() {
+  return (
+    <>
+ 
+    </>
+  );
+}
+
+export default Categories;
