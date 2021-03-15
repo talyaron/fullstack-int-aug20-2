7c9f9a098eec72f0a5b7fcb3359539bc9774027f
Tal <tal.yaron@gmail.com>
2021-03-15 20:31:05 +0200

    redux

---
+++ React/Redux/tal/src/redux/reducers/todo.js
@@ -0,0 +1,18 @@
+const initialState = {todo:[]}
+
+const todoReducer = (state=initialState, action) =>{
+    switch (action.type){
+        case "ADD_TASK":{
+            console.log('add task', action.payload)
+            return {...state, todo:[...state.todo, {id:action.payload.id, taskName:action.payload.taskName}]}
+        }
+        case "DELETE_TASK":{
+            return state
+        }
+        default:{
+            return state
+        }
+    }
+}
+
+export default todoReducer
