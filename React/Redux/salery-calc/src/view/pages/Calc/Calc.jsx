53ba991bf24bd58fe071e2c97ecffa77104562a8
Gilad <Beer>
2021-03-22 20:30:14 +0200

    redux

--- React/Redux/salery-calc/src/view/pages/Calc/Calc.jsx
+++ React/Redux/salery-calc/src/view/pages/Calc/Calc.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams
+  } from "react-router-dom";
+ 
 
 const Calc = props => {
+    let { id } = useParams();
+    const employees = useSelector(state => state.employeesReducer.employees.find(element=>element.id===id));
+    console.log(employees)
+     const handleSubmit = e=>{
+         e.preventDefault()
+         const payPerHour = e.target.children.payPerHour.value
+         const hoursWorked = e.target.children.hoursWorked.value
+         const salery = hoursWorked* payPerHour
+     }
+    
+    
+  
+    
     return (
         <div>
-            
+            <h1>{employees.employeeName}</h1>
+            <form onSubmit={handleSubmit}>
+                <input name='payPerHour' type="number"/>
+                <input name='hoursWorked' type="number"/>
+                <button>submit</button>
+            </form>
         </div>
     )
 }
--- React/Redux/salery-calc/src/view/pages/Employees/Employees.jsx
+++ React/Redux/salery-calc/src/view/pages/Employees/Employees.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  
 
 //components
 import InputName from './InputName/InputName';
@@ -8,11 +15,12 @@ const Employees = () => {
 
     const employees = useSelector(state => state.employeesReducer.employees);
     console.log(employees)
+    
 
     return (
         <div>
             <InputName />
-            {employees.map(({id, employeeName})=> <p key={id}>{employeeName}</p>)}
+            {employees.map(({id, employeeName})=><p key={id}><Link to={id}>{employeeName}</Link></p>)}
         </div>
     )
 }
