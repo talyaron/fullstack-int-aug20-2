12091fcd41c34e747dc690fbb6ae4d5b6afd37fa
OlgaPan-2020 <72758446+OlgaPan-2020@users.noreply.github.com>
2021-04-02 18:24:16 +0300

    1

--- React/Redux/Olga/userPages/src/AllUsers.js
+++ React/Redux/Olga/userPages/src/AllUsers.js
@@ -8,9 +8,14 @@ const AllUsers = () => {
     const dispatch = useDispatch();
     
   // const tasks = useSelector(state => state.todoReducer.todo);
+
+const handleAddUser = (e) =>{
+    const name = e.target
+    console.log(name)
+}
     return(
        <div>
-        <form>
+        <form onSubmit={handleAddUser}>
        <input type='text' placeholder='enter your name' name='name'></input>
        <input type='url' placeholder='image URL'></input>
        <button type='submit'>ADD</button>
