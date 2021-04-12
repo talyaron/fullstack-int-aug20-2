7f967bd88b304a582dae2160f3862e819166be53
eyal shemuel <eshemuel1@gmail.com>
2021-04-12 18:53:59 +0300

    1

---
+++ React/Redux/randomWordGame/cilent/src/redux/Actions/index.js
@@ -0,0 +1,44 @@
+import * as types from "../constants/action-types";
+import axios from "axios";
+
+
+/* export function addWord(payload) {
+  return { type: types.ADD_WORD, payload };
+}
+
+ */
+
+const fetchWordsStart = () => ({
+  type: types.FETCH_WORD_START,
+});
+
+const fetchWordsSuccess = (words) => ({
+  type: types.FETCH_WORD_SUCCESS,
+  payload: words,
+});
+
+const fetchWordsFail = (error) => ({
+  type: types.FETCH_WORD_FAIL,
+  payload: error,
+});
+
+export function fetchWords() {
+   return function (dispatch) {   
+    dispatch(fetchWordsStart());
+    axios
+      .get("https://random-word-api.herokuapp.com//word?number=10")
+      .then((response) => {
+        // const words = response.data;
+        
+        dispatch(fetchWordsSuccess(response.data));
+      }).catch((error)=>
+      dispatch(fetchWordsFail(error.message)))
+  };
+}
+
+
+/* const  getData = async () => {
+  const response = await fetch("https://random-word-api.herokuapp.com//word?number=10");
+  const json = await response.json();
+  return { type: "DATA_LOADED", payload: json };
+} */
