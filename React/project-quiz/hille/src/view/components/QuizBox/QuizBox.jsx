7e53e3717c964db1de69fd70041160ce704574f1
shneur <770abhtur1@gmail.com>
2021-05-31 16:00:02 +0300

    fghjk

--- React/project-quiz/hille/src/view/components/QuizBox/QuizBox.jsx
+++ React/project-quiz/hille/src/view/components/QuizBox/QuizBox.jsx
@@ -1,55 +1,60 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 let quizQestions = [
   {
-      question: "מהו ההר הגבוה בעולם",
-      answer: "האוורסט",
-      answerAll: ["האוורסט","חרמון","חרמון"]
-    },{
-        question: "איזה מתכת נוזלית בטמפרטורת החדר?",
-        answer: "האוורסט",
-        answerAll:["האוורסט","חרמון","חרמון"]
-      },
+    question: "מהו ההר הגבוה בעולם",
+    answer: "האוורסט",
+    answerAll: ["האוורסט", "חרמון", "גבעת התחמושת"],
+  },
+  {
+    question: "איזה מתכת נוזלית בטמפרטורת החדר?",
+    answer: "גליום",
+    answerAll: ["גליום", "מגנזיום", "אלומיניום"],
+  },
 ];
 
-let randomNumber = 0
+let randomNumber = 0;
 
 function QuizBox() {
-const [answers,setanswers] = useState([{
-    question: "איזה מתכת נוזלית בטמפרטורת החדר?",
-    answer: "האוורסט",
-    answerAll:["האוורסט","חרמון","חרמון"]
-  }])
+  const [answers, setanswers] = useState([
+    {
+      question: "איזה מתכת נוזלית בטמפרטורת החדר?",
+      answer: "גליום",
+      answerAll: ["גליום", "מגנזיום", "אלומיניום"],
+    },
+  ]);
 
-useEffect(() => {
-    getAnswer()
-}, [])
+  useEffect(() => {
+    getQuestion();
+  }, []);
 
-
-function getAnswer(){
-    const random = Math.floor(Math.random()*quizQestions.length)
-    console.log(quizQestions[random])
-    if (randomNumber === random){
-        getAnswer()
+  function getQuestion() {
+    const random = Math.floor(Math.random() * quizQestions.length);
+    console.log(quizQestions[random]);
+    if (randomNumber === random) {
+      getQuestion();
+    } else {
+      randomNumber = random;
+      setanswers(quizQestions[random]);
     }
-    else{
-        randomNumber = random
-        setanswers(quizQestions[random])
-    }
-}
+  }
 
-function checkAnswer(){
-
-}
+  function checkAnswer() {}
+  console.log(answers[0])
 
 
   return (
-      <>
-      <button onClick={getAnswer}>שאלה הבאה</button>
-  <div>{answers.question}</div>
-  {answers.answerAll.map((elm,index)=>{
- return(<div key={index} onClick={()=>checkAnswer(elm)}>{elm}</div>)
-  })}
-  </>
+    <>
+      <button onClick={getQuestion}>שאלה הבאה</button>
+      <div>{answers.question}</div>
+      {answers[0].answerAll.map((elm, index) => {
+        return (
+          
+          <div key={index} onClick={() => checkAnswer(elm)}>
+            {elm}
+          </div>
+        );
+      })}
+    </>
   );
 }
 
