03d7a1ed6cccc7d62f92d59285c6127ddbf9d60e
Tal <tal.yaron@gmail.com>
2021-07-15 20:33:33 +0300

    exercise

--- Flutter/basics/Tal/main.dart
+++ Flutter/basics/Tal/main.dart
@@ -1,5 +1,49 @@
-var x = 2;
+//https://www.tutorialspoint.com/dart_programming/dart_programming_html_dom.htm
 
-main() {
-  print(x);
+//number
+import 'class.dart';
+
+double x = 2;
+
+//strings
+String y = "2";
+var e;
+
+bool isTrue = true;
+
+//List
+List arr = [
+  {'name': 'Avi', 'age': 12},
+  {'name': 'Moses', 'age': 45},
+  {'name': 'Bella', 'age': 56},
+  {'name': 'Aka', 'age': 22},
+];
+
+//Map
+var a = 'd';
+// int b = 2;
+Map obj = {a: 1, arr: 2};
+
+//set
+Set mySet = new Set.from(
+    [1, 2, "hi", 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "hi", 1, 1]);
+
+void main() {
+  x = 45.4;
+  // print('x is equal to ${x}');
+  // print(y);
+
+  // for (var student in arr)
+  //   print('${student['name']} is ${student['age']} years old');
+
+  obj[34] = "hi";
+
+  // print(obj);
+  print(mySet);
+  Student ruth = Student('ruth', 21);
+  print(ruth.name);
 }
+
+//create an array of students with object of name and age.
+// print all (for loop) students in the following mannar
+// Moshe id 45 years old
---
+++ Flutter/basics/exercises.md
@@ -0,0 +1,8 @@
+class Transaction
+class Transactions (that stores all transactions)
+
+create bank account with transactions, with description, amount, date (list of transaction)
+1) print all transactions
+2) calculate total amount from start date to end date (method totalSum(startDate, endDate)), under class transactions
+3) find transactions by date
+4) find transactions by range of money transferd
