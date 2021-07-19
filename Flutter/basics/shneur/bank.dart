72f4b0adad33e3519a915fe7cfa71729e460d814
shneur <770abhtur1@gmail.com>
2021-07-19 18:21:21 +0300

    דגכע

--- Flutter/basics/shneur/bank.dart
+++ Flutter/basics/shneur/bank.dart
@@ -1,36 +1,37 @@
-
-import 'footbul.dart';
-
 class Transaction {
-String frome = "";
-String to = "";
-double amount = 0;
-bool income = true;
-String description = "";
-int id = 1;
-DateTime date = new DateTime(221, 06, 5);
+  String frome = "";
+  String to = "";
+  double amount = 0;
+  bool income = true;
+  String description = "";
+  int id = 1;
+  DateTime date = new DateTime(221, 06, 5);
 
-
-
-Transaction(this.frome, this.to, this.amount, this.income, this.description, this.id, this.date);
+  Transaction(this.frome, this.to, this.amount, this.income, this.description,
+      this.id, this.date);
 }
 
- Transaction num1 = Transaction("moshe", "yaakov", 167.4, true, "for the food", 26, new DateTime.now());
-
+Transaction num1 = Transaction(
+    "moshe", "yaakov", 167.4, true, "for the food", 26, new DateTime.now());
 
 int transactionID = 1;
 
- void main() {
-   List <Transaction> transactions = [];
-   transactions.add(Transaction("moshe", "yaakov", 167.4, true, "for the food", 26, new DateTime.now()));
-   for (var obj in transactions){
-     print(obj[num1]);
-};
-   
- }
-CraetTransactionID()=>{
-transactionID = transactionID++
-};
+void main() {
+  List<Transaction> transactions = [];
+  transactions.add(Transaction(
+      "moshe", "yaakov", 167.4, true, "for the food", 26, new DateTime.now()));
+  void printTransaction() {
+    for (Transaction transaction in transactions) {
+      print(transactions[0]);
+    }
+    ;
+
+  }
+  printTransaction();
+}
+
+
+CraetTransactionID() => {transactionID = transactionID++};
 
 // class Transactions (that stores all transactions)
 
