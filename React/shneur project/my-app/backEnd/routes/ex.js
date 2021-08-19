0613c21f147d0b62df9656fd7e769a3558b6f6a5
shneur <770abhtur1@gmail.com>
2021-08-19 15:24:10 +0300

    EXPRESS

---
+++ React/shneur project/my-app/backEnd/routes/ex.js
@@ -0,0 +1,27 @@
+const router = require(`express`).Router();
+let Ex = require(`../models/user.ex`);
+
+router.route("/").get((req, res) => {
+    Ex.find()
+        .then(ex => res.json(ex))
+        .catch(err => res.status(400).json("Eror: " + err));
+});
+
+router.route("/add").post((req, res) => {
+    const username = req.body.username
+    const phoneNumber = Number(req.body.phoneNumber)
+    const email = req.body.email
+  
+    
+
+    const newEx = new Ex({
+        username,
+        phoneNumber,
+        email
+    });
+    newEx.save()
+        .then(() => res.json("ex added"))
+        .catch(err => res.status(400).json("Eror: " + err));
+});
+
+module.exports = router;
--- React/shneur project/my-app/backEnd/server.js
+++ React/shneur project/my-app/backEnd/server.js
@@ -1,15 +1,37 @@
-const express = erquire("express")
-const cors = erquire("cors")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose")
 
-erquire("dotenv").config();
+require("dotenv").config();
 
 
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+const connection = mongoose.connection;
+connection.once(`open`, () => {
+    console.log(`mongoDB is runing`)
+
+})
+
+const exRouter = require("./routes/ex");
+const usersRouter = require("./routes/users")
+
+app.use("/ex", exRouter);
+app.use("/users", usersRouter);
+
+
 
 app.listen(port, () => {
-    console.log(`server is runung on port 4{port}`)
+    console.log(`server is runung on port ${port}`)
 })
